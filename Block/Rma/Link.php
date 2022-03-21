b73c710c522563dffa644c4d2f3df148429c273d
Christoph Aßmann <christoph.assmann@netresearch.de>
2022-03-21 11:04:50 +0100

    DHLGW-1208: list return labels in customer account

---
+++ Block/Rma/Link.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * See LICENSE.md for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Netresearch\ShippingUi\Block\Rma;
+
+use Magento\Customer\Block\Account\SortLink;
+use Magento\Framework\App\DefaultPathInterface;
+use Magento\Framework\View\Element\Template\Context;
+use Netresearch\ShippingUi\ViewModel\Rma\CustomerListing;
+
+class Link extends SortLink
+{
+    /**
+     * @var CustomerListing
+     */
+    private $viewModel;
+
+    public function __construct(
+        Context $context,
+        DefaultPathInterface $defaultPath,
+        CustomerListing $viewModel,
+        array $data = []
+    ) {
+        $this->viewModel = $viewModel;
+
+        parent::__construct($context, $defaultPath, $data);
+    }
+
+    protected function _toHtml(): string
+    {
+        if (!$this->viewModel->getTrackCollection()->getSize()) {
+            return '';
+        }
+
+        return parent::_toHtml();
+    }
+}
